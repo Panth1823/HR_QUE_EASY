8f7e22eb31626dc275872af1fbcab2ba1fc2e1ad
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-05-14 12:24:17 +0530

    initial commit

---
+++ HR Q1.py
@@ -0,0 +1,8 @@
+def solveMeFirst(a, b):
+    return a + b
+
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+res = solveMeFirst(num1, num2)
+print("Sum of", num1, "+", num2, "=", res)
