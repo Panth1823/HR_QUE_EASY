bdda4427dfbd0132b3b4f68d8b946543b274344e
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-05-23 21:20:08 +0530

    updated

---
+++ DSA Python/LIST/Linked List.py
@@ -0,0 +1,7 @@
+# Linked Lists are a data structure that store data in the form of a chain. The structure of a linked list is such that each piece of data has a connection to the next one (and sometimes the previous data as well)
+# Each element in a linked list is called a node.Linked Lists are a data structure that store data in the form of a chain. The structure of a linked list is such that each piece of data has a connection to the next one (and sometimes the previous data as well).
+# Each element in a linked list is called a node.
+# Linked list has ordered elements but it does not have indexes like array
+
+# Doubly Linked List:
+# Contains the address of both the previous element (if one exists) and the next element
