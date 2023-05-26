db014601a906ae158ec4882409b02db80b995fa4
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-05-26 12:35:23 +0530

    updated

---
+++ DSA Python/LIST/Merge Sort.py
@@ -0,0 +1,48 @@
+# It works on Divide and Conquer Approach
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+
+
+input_list = []
+num_elements = int(input("Enter the number of elements: "))
+
+for i in range(num_elements):
+    element = int(input(f"Enter element {i+1}: "))
+    input_list.append(element)
+
+sorted_list = merge_sort(input_list)
+print("Sorted list:", sorted_list)
