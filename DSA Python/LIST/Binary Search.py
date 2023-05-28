a81325b7940c550811fa93a39fa8cc7719839aee
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-05-28 23:08:59 +0530

    updated

--- DSA Python/LIST/Binary Search.py
+++ DSA Python/LIST/Binary Search.py
@@ -1,20 +1,34 @@
-def binarySearch(arey,valuee):
-    start=0
-    end=len(arey)-1
+# def binarySearch(arey,valuee):
+#     start=0
+#     end=len(arey)-1
+#
+#     while start<=end:
+#         mid=(start+end)//2
+#         if arey[mid]==valuee:
+#             return mid
+#         elif arey[mid]<valuee:
+#             start=mid+1
+#         else:
+#             end=mid-1
+#     return -1
+#
+#
+# arey=input("Enter array elements separated with comma:")
+# arey=[int(i) for i in arey.split(",")]
+# valuee=int(input("Enter value to Search:"))
+# result=binarySearch(arey,valuee)
+# print("Index:",result)
 
-    while start<=end:
-        mid=(start+end)//2
-        if arey[mid]==valuee:
-            return mid
-        elif arey[mid]<valuee:
-            start=mid+1
-        else:
-            end=mid-1
-    return -1
-
-
-arey=input("Enter array elements separated with comma:")
-arey=[int(i) for i in arey.split(",")]
-valuee=int(input("Enter value to Search:"))
-result=binarySearch(arey,valuee)
-print("Index:",result)
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+swap=0
+for i in range (0,len(a)):
+    for j in range (0,len(a)-1):
+        if a[j]>a[j+1]:
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
+            swap+=1
+print("Array is sorted in",swap,"swaps.")
+print("First Element:",a[0])
+print("Last Element:",a[len(a)-1])
